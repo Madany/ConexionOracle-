976c1327f931f1df961a508657ddf8094d441582
alumno04 <alumno04@TPP-LAB04-11.TPP.UPEU.EDU.PE>
2015-04-08 11:16:32 -0500

    CONEXXION HECHA

---
+++ src/conexionoracle/Conexion.java
@@ -0,0 +1,24 @@
+
+package conexionoracle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    
+    public static Connection conectar(){
+
+        Connection conexion=null;
+        try {
+           Class.forName("oracle.jdbc.driver.OracleDriver");
+           conexion=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","prueba1","Leydi1234");
+        } 
+        catch (Exception e)
+        {
+        e.printStackTrace();
+        }       
+        return conexion;
+    }
+    
+    
+}
--- src/conexionoracle/ConexionOracle.java
+++ src/conexionoracle/ConexionOracle.java
@@ -1,21 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package conexionoracle;
 
-/**
- *
- * @author alumno04
- */
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ConexionOracle {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        if (Conexion.conectar() == null) {
+            System.out.println("La conexion fallo!!!!!!");
+            
+        } else {
+            System.out.println("La conexion fue exitosa.");
+            
+            try {
+                 Statement st=Conexion.conectar().createStatement();
+                 ResultSet rs=st.executeQuery("SELECT * FROM PERSONA");
+                 while (rs.next()) {
+                     System.out.println("id" +rs.getInt("ID_PERSONA")
+                             +" "+rs.getString("NOMBRE")
+                             +" "+rs.getString("APELLIDO_MAT")
+                             +" "+rs.getString("APELLIDO_PAT"));
+                    
+                }
+            } catch (Exception e) {
+            }
+ 
+           
+        }
     }
     
 }
